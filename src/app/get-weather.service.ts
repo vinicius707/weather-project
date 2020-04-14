d66f7b1cea98fe5cc48fbebcf5394427b1aa72a6
Caio Alves <caiojuodzevicius@gmail.com>
2020-04-14 11:01:35 -0300

    add another fn weather.service

--- src/app/get-weather.service.ts
+++ src/app/get-weather.service.ts
@@ -12,8 +12,20 @@ export class GetWeatherService {
   constructor(private http: HttpClient) { }
 
   getWeather(data) {
+
     let apiWeather = this.baseUrl + data
     return this.http.get(apiWeather)
 
   }
+
+ /*  export class GetWeatherService {
+
+    constructor(private http: HttpClient) { }
+  
+    getWeather(dataCity) {
+  
+      let url = "http://api.weatherbit.io/v2.0/current?" + dataCity + "key=f5d346153e7f4af49fa9d36337a3ab30"
+      return this.http.get(url)
+  
+    } */
 }
