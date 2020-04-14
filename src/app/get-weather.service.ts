d64cd694c6dded59da4a258d79729be9612110ef
Caio Alves <caiojuodzevicius@gmail.com>
2020-04-14 10:46:57 -0300

    add function get url service

--- src/app/get-weather.service.ts
+++ src/app/get-weather.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class GetWeatherService {
 
-  constructor() { }
+  private baseUrl = 'http://api.weatherbit.io/v2.0/current'
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(data) {
+    let apiWeather = this.baseUrl + data
+    return this.http.get(apiWeather)
+
+  }
 }
