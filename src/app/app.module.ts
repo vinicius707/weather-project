0340a256296201d2165035f10d12451d8f7de90a
vinicius707 <viniciussilva707@gmail.com>
2020-04-14 10:28:45 -0300

    Create component search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent
+    DisplayComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
