195b54fca45f865a959d50f620d2c77bf3a2e602
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-15 11:22:09 -0300

    começando a implementar o header api da aplicação

--- src/app/get-weather.service.ts
+++ src/app/get-weather.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +7,22 @@ import { HttpClient } from '@angular/common/http';
 
 export class GetWeatherService {
 
-  private baseUrl = 'http://api.weatherbit.io/v2.0/current'
+  private baseUrl = 'http://api.weatherbit.io/v2.0/current?'
+  private apiKey = "f5d346153e7f4af49fa9d36337a3ab30"
+
 
   constructor(private http: HttpClient) { }
 
-  getWeather(data) {
-
-    let apiWeather = this.baseUrl + data
-    return this.http.get(apiWeather)
+  getWeather() {
+    const headers = new HttpHeaders()
+    .set('content-type','aplication/json')
+    .set('Access-Control-Allow-Origin','*');
+    console.log(headers)
+    let dados = this.http.get(this.baseUrl + "city=SaoPaulo"  + "," + `NC&key=${this.apiKey}`, {'headers':headers})
+    console.log(dados)
+    return this.http.get(this.baseUrl + "city=SaoPaulo"  + "," + `NC&key=${this.apiKey}`, {'headers':headers})
+    // let apiWeather = this.baseUrl + data
+    // return this.http.get(apiWeather)
 
   }
 
