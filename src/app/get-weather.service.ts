0952ce5383f2aa94db01387d854fbf6cb1dcd8f3
ingridfs <ingrid.fernandes.santos@hotmail.com>
2020-04-16 18:35:28 -0300

    Refatorando serviço get-weather

--- src/app/get-weather.service.ts
+++ src/app/get-weather.service.ts
@@ -5,18 +5,22 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root',
 })
 export class GetWeatherService {
-  private apiKey = 'country=BR&lang=pt&key=f5d346153e7f4af49fa9d36337a3ab30';
-  public searchApi;
-  public cityOrPostal;
+
   private baseUrl = `https://api.weatherbit.io/v2.0/current?`;
+  private apiKey = '&country=BR&lang=pt&key=f5d346153e7f4af49fa9d36337a3ab30';
 
   constructor(private http: HttpClient) {}
 
-  getWeather() {
-    const headers = new HttpHeaders()
-      .set('content-type', 'aplication/json')
-      .set('Access-Control-Allow-Origin', '*');
-    console.log(headers);
-    return this.http.get(this.baseUrl+`${this.cityOrPostal}=${this.searchApi}&${this.apiKey}`, { headers: headers });
+  getWeather(city?:string, postalCode?: number) {
+    
+    let cityOrPostal;
+
+    if(city.length != 0) {
+      cityOrPostal = "city=" + city;
+    } else {
+      cityOrPostal = "postal_code=" + postalCode;
+    }
+    let finalUrl = this.baseUrl + cityOrPostal + this.apiKey;
+    return this.http.get(finalUrl);
   }
 }
